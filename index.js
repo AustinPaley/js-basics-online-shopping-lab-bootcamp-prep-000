9c50bb225335a01ef3b39d5606a31251bfb26a83
Austin Paley <austinpaley32@gmail.com>
2018-02-28 18:34:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ else if (cart.length === 1) {
 else if (cart.length === 2){
   console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`)
 }
-else 
+else {
+  for
+}
 }
 
 function total() {
