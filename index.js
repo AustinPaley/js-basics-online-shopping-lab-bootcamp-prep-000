403cb277515583c217ddb14460e62f6979cf951e
Austin Paley <austinpaley32@gmail.com>
2018-02-28 18:49:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ else if (cart.length === 2){
   console.log(`In your cart, you have ${Object.keys(cart[0])} at $${Object.values(cart[0])} and ${Object.keys(cart[1])} at $${Object.values(cart[1])}.`)
 }
 else {
-  for(var i=0; i < cart.length; i++){
+  for(var i=0; i < cart.length-1; i++){
     items.push(`${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`);
   }
   console.log(`In your cart, you have ${items}, and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`)
