6a7ea2c354848aeb5348afa42ff3e7e74d11182a
Austin Paley <austinpaley32@gmail.com>
2018-02-28 18:30:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ if (cart.length === 0){
   console.log("Your shopping cart is empty.")
 } 
 else if (cart.length === 1) {
-  console.log(`In your cart, you have ${Object.`)
+  console.log(`In your cart, you have ${Object.keys[cart][0]`)
   
 }
 }
